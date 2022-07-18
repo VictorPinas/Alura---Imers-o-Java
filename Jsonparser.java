4dbc0f88c9c55d01d858f22af598ebffe65698c7
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-18 15:29:30 -0300

    Jsonparser.java

--- Jsonparser.java
+++ Jsonparser.java
@@ -2,7 +2,24 @@ import java.util.List;
 import java.util.Map;
 
 public class Jsonparser {
-    public List<Map<String, String>> parse(String json) {
+    private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*");
+    private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
 
+    public List<Map<String, String>> parse(String json) {
+        for (String item : items) {
+
+            Map<String, String> atributosItem = new HashMap<>();
+
+            Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
+            while (matcherAtributosJson.find()) {
+                String atributo = matcherAtributosJson.group(1);
+                String valor = matcherAtributosJson.group(2);
+                atributosItem.put(atributo, valor);
+            }
+
+            dados.add(atributosItem);
+        }
+
+        return dados;
     }
 }
