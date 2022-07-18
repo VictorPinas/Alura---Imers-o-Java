0320b0edb9594e2ff4f00df8f5d7475f67bfdc31
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-18 14:52:00 -0300

    body and response

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,7 +10,10 @@ public class App {
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
 
         // extrair só os dados que interessam (título, poster, classificação)
 
