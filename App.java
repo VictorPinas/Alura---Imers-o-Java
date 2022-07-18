395c14916831ad5f9f09ad10fd7ec4ebb8f9d57c
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-18 14:43:37 -0300

    deletar Hello World

--- App.java
+++ App.java
@@ -3,7 +3,6 @@ import java.net.http.HttpRequest;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
