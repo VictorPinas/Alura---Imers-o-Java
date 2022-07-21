79fe991a52591f300a50aeaf5459c35ee682e150
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-07-21 17:00:51 -0300

    ExtratorDeConteudoDaNasa.java

--- ExtratorDeConteudoDaNasa.java
+++ ExtratorDeConteudoDaNasa.java
@@ -1,3 +1,26 @@
-public class ExtratorDeConteudoDaNasa {
-    
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorDeConteudoDaNasa implements ExtratorDeConteudo {
+    public List<Conteudo> extraiConteudos(String json) {
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new Jsonparser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json);
+
+        List<Conteudo> conteudos = new ArrayList<>();
+
+        // popular a lista de conteudos
+        for (Map<String, String> atributos : listaDeAtributos) {
+            String titulo = atributos.get("title");
+            String urlImagem = atributos.get("url");
+
+            var conteudo = new Conteudo(titulo, urlImagem);
+
+            conteudos.add(conteudo);
+        }
+
+        return conteudos;
+    }
 }
